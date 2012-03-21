77886c1afdf39b8f0855c2100840393a47bacd67
Shawn Allen <shawn@stamen.com>
2012-03-20 17:15:35 -0700

    Moved some stuff around

---
+++ js/computed-style.js
@@ -0,0 +1,21 @@
+if (!window.getComputedStyle) {
+    window.getComputedStyle = function(el, pseudo) {
+        this.el = el;
+        this.getPropertyValue = function(prop) {
+            var re = /(\-([a-z]){1})/g;
+            if (prop == 'float') prop = 'styleFloat';
+            if (re.test(prop)) {
+                prop = prop.replace(re, function () {
+                    return arguments[2].toUpperCase();
+                });
+            }
+            return (el && el.currentStyle)
+                ? el.currentStyle[prop]
+                  ? el.currentStyle[prop]
+                  : null
+                : null;
+        };
+        return this;
+    };
+}
+
