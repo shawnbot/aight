3647fc3c370675d7852c83b412386aa25bb9d557
Shawn Allen <shawn@stamen.com>
2012-05-29 16:44:34 -0700

    now with element-properties.js

--- aight.js
+++ aight.js
@@ -1124,6 +1124,23 @@ if (!window.getComputedStyle) {
     };
 }
 
+// from Eli Grey @ http://eligrey.com/blog/post/textcontent-in-ie8
+if (Object.defineProperty && Object.getOwnPropertyDescriptor &&
+    !Object.getOwnPropertyDescriptor(Element.prototype, "textContent").get) {
+    (function() {
+        var innerText = Object.getOwnPropertyDescriptor(Element.prototype, "innerText");
+        Object.defineProperty(Element.prototype, "textContent", {
+            // It won't work if you just drop in innerText.get
+            // and innerText.set or the whole descriptor.
+            get: function() {
+                return innerText.get.call(this)
+            },
+            set: function(x) {
+                return innerText.set.call(this, x)
+            }
+        });
+    })();
+}
 if (!CSSStyleDeclaration.prototype.getProperty) {
     CSSStyleDeclaration.prototype.getProperty = function(a) {
         return this.getAttribute(a);
