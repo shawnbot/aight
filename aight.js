eec60b91f4572ad339fc5b1fc134742cf87c6622
Shawn Allen <shawn@stamen.com>
2012-12-15 00:41:22 -0800

    Firefox Element.prototype.textContent try/catch, v1.0.3

--- aight.js
+++ aight.js
@@ -1,5 +1,5 @@
 /*
- * aight v1.0.2
+ * aight v1.0.3
  *
  * <http://github.com/shawnbot/aight/>
  *
@@ -1329,21 +1329,25 @@ if (!Element.prototype.addEventListener) {
         return result;
     };
 }
-// from Eli Grey @ http://eligrey.com/blog/post/textcontent-in-ie8
-if (Object.defineProperty && Object.getOwnPropertyDescriptor &&
-    Object.getOwnPropertyDescriptor(Element.prototype, "textContent") &&
-    !Object.getOwnPropertyDescriptor(Element.prototype, "textContent").get) {
-    (function() {
-        var innerText = Object.getOwnPropertyDescriptor(Element.prototype, "innerText");
-        Object.defineProperty(Element.prototype, "textContent", {
-            // It won't work if you just drop in innerText.get
-            // and innerText.set or the whole descriptor.
-            get: function() {
-                return innerText.get.call(this)
-            },
-            set: function(x) {
-                return innerText.set.call(this, x)
-            }
-        });
-    })();
-}
+(function() {
+    try {
+        // from Eli Grey @ http://eligrey.com/blog/post/textcontent-in-ie8
+        if (Object.defineProperty && Object.getOwnPropertyDescriptor &&
+            Object.getOwnPropertyDescriptor(Element.prototype, "textContent") &&
+            !Object.getOwnPropertyDescriptor(Element.prototype, "textContent").get) {
+            var innerText = Object.getOwnPropertyDescriptor(Element.prototype, "innerText");
+            Object.defineProperty(Element.prototype, "textContent", {
+                // It won't work if you just drop in innerText.get
+                // and innerText.set or the whole descriptor.
+                get: function() {
+                    return innerText.get.call(this)
+                },
+                set: function(x) {
+                    return innerText.set.call(this, x)
+                }
+            });
+        }
+    } catch (e) {
+        // bad Firefox
+    }
+})();
