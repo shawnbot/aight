a0f116b0fe5e4b5fccf7bbec20b339f69ea9e4a1
Shawn Allen <shawn@stamen.com>
2013-03-18 14:53:39 -0700

    built with event prototype polyfill

--- aight.js
+++ aight.js
@@ -1312,23 +1312,34 @@ if (!document.createElementNS) {
         return document.createElement(name);
     };
 }
-if (!Element.prototype.addEventListener) {
-    Element.prototype.addEventListener = function(type, fn, bubble) {
-        this["e" + type + fn] = fn;
-        var obj = this;
-        this[type + fn] = function onevent() {
-            fn.call(obj, window.event);
-        };
-        return this.attachEvent("on" + type, this[type + fn]);
-    };
-
-    Element.prototype.removeEventListener = function(type, fn) {
-        this["e" + type + fn] = null;
-        var result = this.detachEvent("on" + type, this[type + fn]);
-        this[type + fn] = null;
-        return result;
-    };
-}
+!window.addEventListener && (function (WindowPrototype, DocumentPrototype, ElementPrototype, addEventListener, removeEventListener, dispatchEvent, registry) {
+    WindowPrototype[addEventListener] = DocumentPrototype[addEventListener] = ElementPrototype[addEventListener] = function (type, listener) {
+		var target = this;
+ 
+		registry.unshift([target, type, listener, function (event) {
+			event.currentTarget = target;
+			event.preventDefault = function () { event.returnValue = false };
+			event.stopPropagation = function () { event.cancelBubble = true };
+			event.target = event.srcElement || target;
+ 
+			listener.call(target, event);
+		}]);
+ 
+		this.attachEvent("on" + type, registry[0][3]);
+	};
+ 
+	WindowPrototype[removeEventListener] = DocumentPrototype[removeEventListener] = ElementPrototype[removeEventListener] = function (type, listener) {
+		for (var index = 0, register; register = registry[index]; ++index) {
+			if (register[0] == this && register[1] == type && register[2] == listener) {
+				return this.detachEvent("on" + type, registry.splice(index, 1)[0][3]);
+			}
+		}
+	};
+ 
+	WindowPrototype[dispatchEvent] = DocumentPrototype[dispatchEvent] = ElementPrototype[dispatchEvent] = function (eventObject) {
+		return this.fireEvent("on" + eventObject.type, eventObject);
+	};
+})(Window.prototype, HTMLDocument.prototype, Element.prototype, "addEventListener", "removeEventListener", "dispatchEvent", []);
 (function() {
     try {
         // from Eli Grey @ http://eligrey.com/blog/post/textcontent-in-ie8
