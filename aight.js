3eceba8b0e94146119c42a8a689640d22260b51a
Usman Akeju <akeju00+github@gmail.com>
2014-05-22 19:31:22 +0200

    Build aight.js and minified file

--- aight.js
+++ aight.js
@@ -1759,19 +1759,40 @@ if (!document.createElementNS) {
 })(Window.prototype, HTMLDocument.prototype, Element.prototype, "addEventListener", "removeEventListener", "dispatchEvent", []);
 (function() {
     try {
-        // from Eli Grey @ http://eligrey.com/blog/post/textcontent-in-ie8
+        // inspired by Eli Grey's shim @ http://eligrey.com/blog/post/textcontent-in-ie8
+        // heavily modified to better match the spec:
+        // http://www.w3.org/TR/2004/REC-DOM-Level-3-Core-20040407/core.html#Node3-textContent
         if (Object.defineProperty && Object.getOwnPropertyDescriptor &&
             Object.getOwnPropertyDescriptor(Element.prototype, "textContent") &&
             !Object.getOwnPropertyDescriptor(Element.prototype, "textContent").get) {
+            var nodeValue = Object.getOwnPropertyDescriptor(Text.prototype, "nodeValue");
+            Object.defineProperty(Text.prototype, "textContent", {
+                // It won't work if you just drop in nodeValue.get
+                // and nodeValue.set or the whole descriptor.
+                get: function() {
+                    return nodeValue.get.call(this);
+                },
+                set: function(x) {
+                    return nodeValue.set.call(this,x);
+                }
+            });
             var innerText = Object.getOwnPropertyDescriptor(Element.prototype, "innerText");
             Object.defineProperty(Element.prototype, "textContent", {
-                // It won't work if you just drop in innerText.get
-                // and innerText.set or the whole descriptor.
                 get: function() {
+                    // It won't work if you just drop in innerText.get
+                    // or the whole descriptor.
                     return innerText.get.call(this);
                 },
                 set: function(x) {
-                    return innerText.set.call(this, x);
+                    //return innerText.set.call(this,x); // nope! you do weird things!
+                    var c;
+                    while(!!(c=this.firstChild)) { this.removeChild(c); }
+                    if (x!==null) {
+                        c=document.createTextNode(x);
+                        this.appendChild(c);
+                    }
+                    c=null;
+                    return x;
                 }
             });
         }
