c2ced1a5e15a5931a9145496d531f356dc0d0cfd
Usman Akeju <akeju00+github@gmail.com>
2014-06-11 19:23:30 +0200

    Flesh out Node.textContent shim a bit more
    Now works with non-text, non-element nodes (e.g. comment, document
    fragments, etc.) as well as weird IE elements that somehow have
    different behaviors (e.g. script, style, etc.).
    
    Fixes shawnbot/aight#27.

--- aight.js
+++ aight.js
@@ -1765,36 +1765,115 @@ if (!document.createElementNS) {
         if (Object.defineProperty && Object.getOwnPropertyDescriptor &&
             Object.getOwnPropertyDescriptor(Element.prototype, "textContent") &&
             !Object.getOwnPropertyDescriptor(Element.prototype, "textContent").get) {
-            var nodeValue = Object.getOwnPropertyDescriptor(Text.prototype, "nodeValue");
-            Object.defineProperty(Text.prototype, "textContent", {
-                // It won't work if you just drop in nodeValue.get
-                // and nodeValue.set or the whole descriptor.
-                get: function() {
-                    return nodeValue.get.call(this);
-                },
-                set: function(x) {
-                    return nodeValue.set.call(this,x);
-                }
-            });
+
+            // NOTE: Neither of these "drop-in" patterns would work:
+            // Object.defineProperty(..., ..., descriptor); // nope!
+            // Object.defineProperty(..., ..., { get: descriptor.get, set: descriptor.set }); // nope!
+            // So must use function-wrapped descriptor.fn.call pattern.
+
+            // "Normal" Elements
             var innerText = Object.getOwnPropertyDescriptor(Element.prototype, "innerText");
-            Object.defineProperty(Element.prototype, "textContent", {
-                get: function() {
-                    // It won't work if you just drop in innerText.get
-                    // or the whole descriptor.
-                    return innerText.get.call(this);
-                },
-                set: function(x) {
-                    //return innerText.set.call(this,x); // nope! you do weird things!
+            var setTextContent = function(x) {
+                    // Note that setting textContent is different from innerText, so
+                    // calling innerText.set would be incorrect.
                     var c;
-                    while(!!(c=this.firstChild)) { this.removeChild(c); }
+                    while(!!(c=this.lastChild)) {
+                        this.removeChild(c);
+                    }
                     if (x!==null) {
                         c=document.createTextNode(x);
                         this.appendChild(c);
                     }
-                    c=null;
                     return x;
+            };
+            Object.defineProperty(Element.prototype, "textContent", {
+                get: function() {
+                    return innerText.get.call(this);
+                },
+                set: function(x) {
+                    return setTextContent.call(this, x);
                 }
             });
+	    // <script> Elements
+            var scriptText = Object.getOwnPropertyDescriptor(HTMLScriptElement.prototype, "text");
+            Object.defineProperty(HTMLScriptElement.prototype, "textContent", {
+                get: function() {
+                    return scriptText.get.call(this);
+                },
+                set: function(x) {
+                    return scriptText.set.call(this, x);
+                }
+            });
+	    // <style> Elements
+            var cssText = Object.getOwnPropertyDescriptor(CSSStyleSheet.prototype, "cssText");
+            Object.defineProperty(HTMLStyleElement.prototype, "textContent", {
+                get: function() {
+                    return cssText.get.call(this.styleSheet);
+                },
+                set: function(x) {
+                    return cssText.set.call(this.styleSheet, x);
+                }
+            });
+	    // <title> Elements
+            var titleText = Object.getOwnPropertyDescriptor(HTMLTitleElement.prototype, "text");
+            Object.defineProperty(HTMLTitleElement.prototype, "textContent", {
+                get: function() {
+                    return titleText.get.call(this);
+                },
+                set: function(x) {
+                    return titleText.set.call(this, x);
+                }
+            });
+            // Text nodes
+            var textNodeValue = Object.getOwnPropertyDescriptor(Text.prototype, "nodeValue");
+            Object.defineProperty(Text.prototype, "textContent", {
+                get: function() {
+                    return textNodeValue.get.call(this);
+                },
+                set: function(x) {
+                    return textNodeValue.set.call(this, x);
+                }
+            });
+            // Comments (and possibly other weird Node types that are treated as comments in IE)
+            var elementNodeValue = Object.getOwnPropertyDescriptor(Element.prototype, "nodeValue");
+            Object.defineProperty(HTMLCommentElement.prototype, "textContent", {
+                get: function() {
+                    return elementNodeValue.get.call(this);
+                },
+                set: function(x) {
+                    return elementNodeValue.set.call(this, x);
+                }
+            });
+            // Document and DocumentFragment Nodes
+            // NOTE: IE8 seems to reuse HTMLDocument for both, so have to check nodeType explicitly
+            var documentNodeValue = Object.getOwnPropertyDescriptor(HTMLDocument.prototype, "nodeValue");
+            Object.defineProperty(HTMLDocument.prototype, "textContent", {
+                get: function() {
+                    // document fragments have textContent
+                    if (this.nodeType === 11) {
+                        var c = this.firstChild;
+                        var tc=[];
+                        // append the textContent of its children
+                        while(!!c) {
+                            tc.push(c.textContent);
+                            c = c.nextSibling;
+                        }
+                        c = null;
+                        tc = tc.join('');
+                        return tc;
+                    }
+                    // a true Document's textContent is always null
+                    return null; // === documentNodeValue.get.call(this);
+                },
+                set: function(x) {
+                    if (this.nodeType === 11) {
+                        return setTextContent.call(this, x);
+                    }
+                    // setting a Document's textContent has no side effects
+                    return x; // === documentNodeValue.set.call(this, x);
+                }
+            });
+            // other Node types are either deprecated or don't matter in HTML5/IE standards mode
         }
     } catch (e) {
         // bad Firefox
