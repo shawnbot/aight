27b1fffd77c07bac860d0f072bbbff510a78ca3e
Shawn Allen <shawn.allen@hush.com>
2014-09-12 21:20:27 -0700

    source files

---
+++ src/start.js
@@ -0,0 +1,6 @@
+/*
+ * aight <http://github.com/shawnbot/aight/>
+ * is a collection of JavaScript shims and polyfills that make IE8 (and other
+ * legacy browsers) behave a lot more like modern ones.
+ */
+(function() {
