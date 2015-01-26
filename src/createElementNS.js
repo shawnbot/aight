d1f97bfa908290d34a29f47381db3424358209c7
Shawn Allen <shawn.allen@gsa.gov>
2015-01-26 10:58:54 -0800

    assume that the HTMLDocument interface exists

--- src/createElementNS.js
+++ src/createElementNS.js
@@ -1,8 +1,5 @@
 if (!('createElementNS' in document)) {
-  var DocumentPrototype = (typeof Document === "function")
-    ? Document.prototype
-    : HTMLDocument.prototype;
-  DocumentPrototype.createElementNS = function(ns, name) {
+  window.HTMLDocument.createElementNS = function(ns, name) {
     if (ns) throw "sorry, this browser does not support namespaces";
     return DocumentPrototype.createElement.call(this, name);
   };
