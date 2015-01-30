919574858c611b53853c7d7b64207234aa2db85b
Shawn Allen <shawn.allen@gsa.gov>
2015-01-29 17:36:54 -0800

    only shim opacity in IE8

--- src/d3.ie8.js
+++ src/d3.ie8.js
@@ -3,9 +3,7 @@
  */
 ;(function(exports) {
 
-  aight.d3 = {
-    version: aight.version
-  };
+  aight.d3 = {};
 
   var aight_mappedProperty = function(property, format, parse) {
     var read = function(_property) {
@@ -42,17 +40,7 @@
   };
 
   // a map of shimmed CSS properties
-  var aight_d3_style = {
-    "opacity": aight_mappedProperty("filter",
-      function opacity_to_filter(opacity) {
-        if (isNaN(opacity)) opacity = 100;
-        return ["alpha(opacity=", Math.round(opacity * 100), ")"].join("");
-      }, 
-      function filter_to_opacity(filter) {
-        var match = (filter || "").match(/alpha\(opacity=(\d+)\)/);
-        return match ? match[1] / 100 : 1;
-      })
-  };
+  var aight_d3_style = {};
 
   aight.d3.style = aight_d3_style;
   aight.d3.mappedProperty = aight_mappedProperty;
@@ -65,5 +53,17 @@
       : d3_style;
     return style.apply(this, arguments);
   };
+
+  if (aight.browser.ie8) {
+    aight_d3_style.opacity = aight_mappedProperty("filter",
+      function opacity_to_filter(opacity) {
+        if (isNaN(opacity)) opacity = 100;
+        return ["alpha(opacity=", Math.round(opacity * 100), ")"].join("");
+      },
+      function filter_to_opacity(filter) {
+        var match = (filter || "").match(/alpha\(opacity=(\d+)\)/);
+        return match ? match[1] / 100 : 1;
+      });
+  }
 
 })(this);
